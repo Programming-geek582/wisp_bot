fb8c028dec0451deef3d86f65df60c93c829e096
Heckler <77711811+Programming-geek582@users.noreply.github.com>
2022-04-08 15:08:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import nextcord
+import time
+import webserver
+from dislevel import increase_xp
+from nextcord.ext import commands
+
+bot = commands.Bot(command_prefix=['wisp ', 'w!', 'w.', '!w'], intents=nextcord.Intents.all(), owner_ids=[837730346874306581, 824236988433039391], case_insensitive=True)
+
+bot.lavalink_nodes = [
+	{"host": "losingtime.dpaste.org", "port": 2124, "password": "SleepingOnTrains"},
+	{"host": "lava.link", "port": 80, "password": "dismusic"},
+	{"host": "lavalink.islantay.tk", "port": 8880, "password": "waifufufufu"}
+]
+
+extensions = ['extensions.image','extensions.mod','extensions.owner','extensions.info', 'extensions.handlers', 'extensions.help', 'extensions.minigames', 'extensions.fun', 'dismusic', 'extensions.utils', 'jishaku', 'dislevel']
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to discord')
+    print(f'Bot guilds count: {len(bot.guilds)}')
+    for cog in extensions:
+    	try:
+	    	bot.load_extension(cog)
+	    	print(f'Loaded cog {cog}')
+    	except Exception as e:
+	    	raise e
+
+@bot.event
+async def on_message(message : nextcord.Message):
+    if message.author == bot.user:
+        return
+    
+    if message.content.isupper == True:
+        if message.author.id == bot.owner_id:
+            return
+        else:
+            await message.reply('Stop sending messages in uppercase nab')
+    if not message.author.bot:
+        await increase_xp(message, bot, rate=5)
+        time.sleep(5)
+		
+    await bot.process_commands(message)
+
+webserver.keep_alive()
+bot.run(os.getenv("TOKEN"))
