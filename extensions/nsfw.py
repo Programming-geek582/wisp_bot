23334b5a664a4d1b62add41c8d82df07a76ad613
Heckler <77711811+Programming-geek582@users.noreply.github.com>
2022-04-14 18:04:32 +0530

    Added a ton of nsfw cmds
    This cog is nsfw only. no copying my code or imma get you gbanned.

--- extensions/nsfw.py
+++ extensions/nsfw.py
@@ -20,6 +20,60 @@ class NSFW(commands.Cog, name="nsfw"):
         embed.set_image(url=res['url'])
         await ctx.send(embed=embed)
 
+    @commands.command()
+    @commands.is_nsfw()
+    async def yuri(self, ctx : commands.Context):
+        r = requests.get('https://nekos.life/api/v2/img/yuri')
+        res = r.json()
+        embed = nextcord.Embed(title="Yuri", colour=0xff0000)
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
 
+    @commands.command()
+    @commands.is_nsfw()
+    async def solog(self, ctx : commands.Context):
+        r = requests.get('https://nekos.life/api/v2/img/solog')
+        res = r.json()
+        embed = nextcord.Embed(title="Solog", colour=0xff0000)
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.is_nsfw()
+    async def smug(self, ctx : commands.Context):
+        r = requests.get('https://nekos.life/api/v2/img/smug')
+        res = r.json()
+        embed = nextcord.Embed(title="Hentai", colour=0xff0000)
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    @commands.is_nsfw()
+    async def feet(self, ctx : commands.Context):
+        r = requests.get('https://nekos.life/api/v2/img/feet')
+        res = r.json()
+        embed = nextcord.Embed(title="Hentai", colour=0xff0000)
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.is_nsfw()
+    async def smallboobs(self, ctx : commands.Context):
+        r = requests.get('https://nekos.life/api/v2/img/smallboobs')
+        res = r.json()
+        embed = nextcord.Embed(title="Hentai", colour=0xff0000)
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    @commands.is_nsfw()
+    async def hentai(self, ctx : commands.Context):
+        r = requests.get('https://nekos.life/api/v2/img/hentai')
+        res = r.json()
+        embed = nextcord.Embed(title="Hentai", colour=0xff0000)
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
+
+        
 def setup(bot):
     bot.add_cog(NSFW(bot))
