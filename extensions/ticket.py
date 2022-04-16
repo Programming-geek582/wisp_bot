46717294cd2f03e9f0e4d16432ba9eb822f24f0b
Heckler <77711811+Programming-geek582@users.noreply.github.com>
2022-04-16 11:40:06 +0530

    Added ticket system

---
+++ extensions/ticket.py
@@ -0,0 +1,37 @@
+import nextcord
+import aiosqlite
+from .ticket_view import ticket_create_view
+from nextcord.ext import commands
+
+class Tickets(commands.Cog, name="tickets"):
+	"""Ticket system for wisp bot"""
+	def __init__(self, bot : commands.Bot):
+		self.bot = bot
+
+	@commands.Cog.listener()
+	async def on_ready():
+		async with aiosqlite.connect('tickets.db') as db:
+			async with db.cursor() as cursor:
+				await cursor.execute('CREATE TABLE IF NOT EXISTS tickets(guild_id INT, category TEXT, counter INT, channel INT)')
+
+		await db.commit()
+
+	@commands.command()
+	@commands.cooldown(1, 30, commands.BucketType.user)
+	@commands.has_permissions(administrator=True)
+	async def tickets(self, ctx : commands.Context):
+		async with aiosqlite.connect('tickets.db') as db:
+			async with db.cursor() as cursor:
+				await cursor.execute('SELECT category, channel FROM tickets WHERE guild_id = ?', (ctx.guild.id,))
+				data = await cursor.fetchone()
+				if data:
+					await cursor.execute('UPDATE tickets SET category = ? AND counter = ? AND channel ? WHERE guild_id = ?', (ctx.channel.category.name, 0, ctx.channel.id, ctx.guild.id))
+				else:
+					await cursor.execute('INSERT INTO tickets(guild_id, category, counter, channel) VALUES (?, ?, ?, ?)', (ctx.guild.id, ctx.channel.category.name, 0, ctx.channel.id))
+		await db.commit()
+		await ctx.send('Tickets have successfully setup. now click the button to test out the ticket system', delete_after=10)
+		embed = nextcord.Embed(title="Create a ticket", description="Create a ticket to contact the server mods, or to report something or whatever you want.", colour=0xff0000)
+		await ctx.send(embed=embed)
+
+def setup(bot : commands.Bot):
+	bot.add_cog(Tickets(bot))
---
+++ extensions/ticket_view.py
@@ -0,0 +1,17 @@
+import nextcord
+import aiosqlite
+from typing import Optional
+
+class ticket_create_view(nextcord.ui.View):
+    def __init__(self, *, timeout: Optional[float] = 180):
+        super().__init__(timeout=timeout)
+
+    @nextcord.ui.Button()
+    async def create_ticket(self, button : nextcord.ui.Button, interaction : nextcord.Interaction):
+        async with aiosqlite.connect('tickets.db') as db:
+            async with db.cursor() as cursor:
+                await cursor.execute('SELECT counter FROM tickets WHERE guild_id = ?', (interaction.guild.id))
+                data = await cursor.fetchone()
+
+        await db.commit()
+        await interaction.channel.category.create_text_channel(name=f"ticket {data[0]} - {interaction.user.name}")
